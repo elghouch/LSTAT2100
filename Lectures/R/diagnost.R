45643ad15fb4f006e988ea00b7b074b89ec82a9e
Anour El Ghouch <anouar.elghouch@uclouvain.be>
2024-01-30 10:45:33 +0100

    update

--- Lectures/R/diagnost.R
+++ Lectures/R/diagnost.R
@@ -1,5 +1,21 @@
-if (!require(statmod)) install.packages(statmod)
-diagnost <- function(mod, type = "rqr", plots = c("response", "fitted", "qqplot", "FormulaVar", "DataVar")) {
+if (!require(statmod)) install.packages("statmod")
+
+# mod: the glm model to diagnose
+#
+# type: the type of residuals to be calculated. This can be
+#   #"rqr", for "randomized quantile residuals" (default). These are calculated using statmod::qresiduals() 
+#   #"deviance", or "pearson", or any other type of residuals from residuals()
+#
+# plots: Which plots to show? This can be 
+#   #"response", for reponses ~ fitteds 
+#   #"fitted", for residuals ~ fitteds 
+#   #"qqplot", to make a QQ-plot
+#   #"formula", residuals ~ term, for all "terms" (covariates) appearing in the model formula
+#   #"data", residuals ~ var, for all numerical covariates appearing in the provided data
+#   #FALSE, return the residuals, do not plot
+#   #a character vector of the names of the variables (in the provided data) against which the residuals are to be plotted
+
+diagnost <- function(mod, type = "rqr", plots = c("response", "fitted", "qqplot", "formula", "data")) {
   if (type == "rqr") {
     prsd <- statmod::qresiduals(mod) |> pnorm()
     prsd[prsd == 1] <- 1 - 1e-010
@@ -16,9 +32,13 @@ diagnost <- function(mod, type = "rqr", plots = c("response", "fitted", "qqplot"
   if (length(dev.list()) == 0) dev.new()
   opar <- par(no.readonly = TRUE)
   par(mar = c(3, 3, 1, 1), mgp = c(1.5, 0.5, 0), pch = 20)
+  if ("formula" %in% plots) plots <- c(plots, attr(terms(mod), "term.labels")) |> unique()
+  
   NumVarNames <- colnames(Filter(is.numeric, mod$data))
-  if ("FormulaVar" %in% plots) plots <- c(plots, attr(terms(mod), "term.labels")) |> unique()
-  if ("DataVar" %in% plots) plots <- c(plots, NumVarNames[-which(NumVarNames == all.vars(formula(mod))[1])]) |> unique()
+  resops <- all.vars(formula(mod))[1]
+  if (resops %in% NumVarNames) NumVarNames <- NumVarNames[-which(NumVarNames == resops)]
+  if ("data" %in% plots) plots <- c(plots, NumVarNames) |> unique()
+  
   if (length(plots) > 1) {
     par(mfrow = c(1, 2))
   } else {
@@ -30,7 +50,7 @@ diagnost <- function(mod, type = "rqr", plots = c("response", "fitted", "qqplot"
       abline(a = 0, b = 1, col = "blue")
       mtext(side = 3, line = 0, adj = 0, cex = 0.8, paste("Response vs Fitted"), font = 2)
     } else if (plots[i] == "fitted") {
-      scatter.smooth(fitted(mod), rsd, lpars = list(col = gray(0.6)), main = NULL, xlab = "Predicted Values", ylab = "Residuals")
+      scatter.smooth(fitted(mod), rsd, degree = 2, lpars = list(col = gray(0.6)), main = NULL, xlab = "Predicted Values", ylab = "Residuals")
       abline(h = 0, col = "blue")
       mtext(side = 3, line = 0, adj = 0, cex = 0.8, paste(type, ":", "Residuals vs Fitted"), font = 2)
     } else if (plots[i] == "qqplot") {
@@ -38,11 +58,11 @@ diagnost <- function(mod, type = "rqr", plots = c("response", "fitted", "qqplot"
       qqline(rsd, col = "blue")
       mtext(side = 3, line = 0, adj = 0, cex = 0.8, paste(type, ":", "QQ-plot"), font = 2)
     } else if (plots[i] %in% names(mod$model)) {
-      scatter.smooth(mod$model[, plots[i]], rsd, lpars = list(col = gray(0.6)), xlab = plots[i], main = NULL, ylab = "Residuals")
+      scatter.smooth(mod$model[, plots[i]], rsd, degree = 2, lpars = list(col = gray(0.6)), xlab = plots[i], main = NULL, ylab = "Residuals")
       abline(h = 0, col = "blue")
       mtext(side = 3, line = 0, adj = 0, cex = 0.8, paste(type, ":", "Residuals vs", plots[i]), font = 2)
     } else if (plots[i] %in% names(mod$data)) {
-      scatter.smooth(mod$data[, plots[i]], rsd, xlab = plots[i], main = NULL, ylab = "Residuals")
+      scatter.smooth(mod$data[, plots[i]], rsd, degree = 2, xlab = plots[i], main = NULL, ylab = "Residuals")
       abline(h = 0, col = "blue")
       mtext(side = 3, line = 0, adj = 0, cex = 0.8, paste(type, ":", "Residuals vs", plots[i]), font = 2)
     }
--- Lectures/R/pr2.R
+++ Lectures/R/pr2.R
@@ -1,3 +1,4 @@
+# ... : the name of one or more glm models for which Psudo-R2 is to be calculated
 pr2 <- function(...) {
   list(...) |> sapply(\(glm) (1 - glm$deviance / glm$null.deviance) * 100)
 }
